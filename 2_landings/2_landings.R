20197cb74b6a4d087f34d8014370b85c3dfa08d4
Christopher <christopher.i.huggins@gmail.com>
2018-08-01 15:08:20 +0100

    Summary landings charts

---
+++ 2_landings/2_landings.R
@@ -0,0 +1,33 @@
+library(readxl)
+library(tidyverse)
+library(hrbrthemes)
+
+landings <- read_xlsx("data/Published_dataset_2017.xlsx")
+
+uk_nations <- c("England", "Northern Ireland", "Scotland", "Wales")
+
+landings_overview <- landings %>%
+  filter(`Port Nationality` %in% uk_nations) %>%
+  group_by(`Port Nationality`) %>%
+  summarise(total_weight = sum(`Landed weight (tonnes)`), total_value = sum(`Value(£000s)`))
+
+plot_landings_weight <- ggplot(landings_overview, aes(y = total_weight/1000, x = `Port Nationality`)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = format(round(total_weight, digits = 0), big.mark = ",")), vjust = -0.15, fontface = 2, size = 3) +
+  theme_ipsum(plot_margin = margin(7,7,7,7)) +
+  theme(panel.grid.minor = element_blank(), panel.grid.major.x = element_blank()) +
+  labs(title = "Total landings by weight",
+       y = "Total landings (thousand tonnes)",
+       x = NULL)
+ggsave(plot_landings_weight, file = "2_landings/plot_landings_weight.png", height = 8, width = 14, units = "cm", dpi = 1200)
+
+plot_landings_value <- ggplot(landings_overview, aes(y = total_value/1000, x = `Port Nationality`)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = paste("£", format(round((total_value * 1000), digits = 0), big.mark = ","))), vjust = -0.15, fontface = 2, size = 3) +
+  theme_ipsum(plot_margin = margin(7,7,7,7)) +
+  theme(panel.grid.minor = element_blank(), panel.grid.major.x = element_blank()) +
+  labs(title = "Total landings by value",
+       y = "Total landings (£millions)",
+       x = NULL)
+ggsave(plot_landings_value, file = "2_landings/plot_landings_value.png", height = 8, width = 14, units = "cm", dpi = 1200)
+
